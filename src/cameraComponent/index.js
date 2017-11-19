a051fcd69be58c3cca2a3e74db62aa29a934cf7d
hiby90hou <hiby.90hou@qq.com>
2017-11-19 12:18:03 +1100

    add scan bar showing

--- src/cameraComponent/index.js
+++ src/cameraComponent/index.js
@@ -6,13 +6,20 @@ import {
   AppRegistry,
   Dimensions,
   TouchableHighlight,
-  Button
+  Button,
+  Animated,
+  Easing
 } from 'react-native';
 
  import Camera from 'react-native-camera';
 
 class cameraComponent extends Component {
-
+    constructor(props) {
+        super(props)
+        this.state = {
+            moveAnim: new Animated.Value(0)
+        }
+    }
 
   takePicture = () => {
     const {changeCameraState} = this.props
@@ -31,7 +38,25 @@ class cameraComponent extends Component {
         // qrCodeRead && qrCodeRead(data);
         alert('BarCodeRead:'+result.data)
         changeCameraState()
-    };
+    }
+
+     componentDidMount() {
+        this.startAnimation()
+        console.log('startAnimation')
+    }
+
+    startAnimation = () => {
+        this.state.moveAnim.setValue(0);
+        Animated.timing(
+            this.state.moveAnim,//init
+            {
+                toValue: -200,
+                duration: 1500,
+                easing: Easing.linear
+            }//end
+        ).start(() => this.startAnimation())//start
+    }
+
 
   render() {
 
@@ -74,18 +99,24 @@ class cameraComponent extends Component {
         justifyContent: 'center',
         backgroundColor: 'transparent'
         },
-        rectangle: {
-            height: 200,
-            width: 200,
-            borderWidth: 1,
-            borderColor: '#00FF00',
-            backgroundColor: 'transparent'
-        },
-        rectangleText: {
-            flex: 0,
-            color: '#fff',
-            marginTop: 10
-        }
+      rectangle: {
+          height: 200,
+          width: 200,
+          borderWidth: 1,
+          borderColor: '#00FF00',
+          backgroundColor: 'transparent'
+      },
+      rectangleText: {
+          flex: 0,
+          color: '#fff',
+          marginTop: 10
+      },
+    border: {
+        flex: 0,
+        width: 200,
+        height: 2,
+        backgroundColor: '#00FF00'
+    }
     })
 
 
@@ -102,7 +133,9 @@ class cameraComponent extends Component {
               aspect={Camera.constants.Aspect.fill}>
               <View style={styles.rectangleContainer}>
                   <View style={styles.rectangle}/>
-
+                  <Animated.View style={[
+                      styles.border,
+                      {transform: [{translateY: this.state.moveAnim}]}]}/>
                   <Text style={styles.rectangleText}>put your bar code into this rectangle</Text>
                   <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
               </View>
