80ebac8814c28bd46c7e3cbb23d568249af4506f
hiby90hou <hiby.90hou@qq.com>
2017-12-27 18:27:28 +1100

    write comments

--- backend_server/myshoppinglist_server/app/controllers/api/v1/users_controller.rb
+++ backend_server/myshoppinglist_server/app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,13 @@
 module Api
 	module V1
 		class UsersController < ApplicationController
+			# use GET "http://localhost:3000/api/v1/users/"
 			def index
 				users = User.order('created_at DESC')
 				render json: {status: 'SUCCESS', message:'Loaded shoppinglists', data:users},status: :ok
 			end
 
+			# use GET "http://localhost:3000/api/v1/users/Richard%20Burke"
 			def show
 				user = User.find_by_user_name(params[:id])
 				if user
@@ -14,6 +16,16 @@ module Api
 					render json: {status: 'ERROR', message:'shoppinglist cannot find', data:"error"},status: :unprocessable_entity	
 				end
 			end
+
+			# use POST "http://localhost:3000/api/v1/users"
+			# head Content-Type : application/json
+			# body
+			# {
+	 	  #   	 "todos": "[{'is_done':'false',title:'Chicken wings3',expire:null},{is_done:false,title:'Scotch eggs2',expire:null},{is_done:'false','title':'Tiramisù2','expire':'null'}]",
+   		#      "is_all_done": "false",
+   		#      "user_name": "rachel_green2",
+			# 	   "password": "12345"
+			# }
 
 			def create
 				user = User.new(user_params)
@@ -25,16 +37,24 @@ module Api
  				end
 			end
 
+			# use DELETE "http://localhost:3000/api/v1/users/rachel_green2"
+
 			def destroy
 				user = User.find_by_user_name(params[:id])
 				if user
 					user.destroy
 					render json: {status: 'SUCCESS', message:'Deleted shoppinglist', data:user}, status: :ok
 				else
-					render json: {status: 'ERROR', message:'shoppinglist cannot find', data:"cannot find shopping list, so I cannot delete it."},status: :unprocessable_entity	
-
+					render json: {status: 'ERROR', message:'shoppinglist cannot find', data:"cannot find shopping list, so I cannot delete it."},status: :unprocessable_entity
 				end
 			end
+
+			# use PUT "http://localhost:3000/api/v1/users/Richard%20Burke"
+			# head Content-Type : application/json
+			# body
+			# {
+			# 	"todos":"[{'is_done':'false',title:'Chicken wings4',expire:null}]"
+			# }
 
 			def update
 				user = User.find_by_user_name(params[:id])
